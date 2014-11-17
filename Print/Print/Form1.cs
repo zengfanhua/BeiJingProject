d39355838aa894f983c262315ea0c207d51a77de
fengchao <fc457@sina.com>
2014-11-17 14:08:29 +0800

    123

--- Print/Print/Form1.cs
+++ Print/Print/Form1.cs
@@ -19,7 +19,7 @@ namespace Print
         List<PrintInfo> list = new List<PrintInfo>();
 
         /// <summary>
-        /// 页面加载
+        /// 页面加载test
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
